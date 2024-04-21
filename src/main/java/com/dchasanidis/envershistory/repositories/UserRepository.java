4ea5718225b348ed2dc0e61ecd199e42b76002ea
Dimitris Chasanidis <dchasanidis@dol-guldur.speedport.ip>
2024-04-21 18:31:02 +0200

    add user repository

--- src/main/java/com/dchasanidis/envershistory/repositories/UserRepository.java
+++ src/main/java/com/dchasanidis/envershistory/repositories/UserRepository.java
@@ -1,2 +1,11 @@
-package com.dchasanidis.envershistory.repositories;public interface UserRepository {
+package com.dchasanidis.envershistory.repositories;
+
+import com.dchasanidis.envershistory.entities.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 }
