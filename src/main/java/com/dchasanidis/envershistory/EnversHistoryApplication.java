1f5bcf8788f38e7bdef52001075420f7efee25a9
Dimitris Chasanidis <dchasanidis@dol-guldur.speedport.ip>
2024-04-21 18:33:09 +0200

    format

--- src/main/java/com/dchasanidis/envershistory/EnversHistoryApplication.java
+++ src/main/java/com/dchasanidis/envershistory/EnversHistoryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EnversHistoryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EnversHistoryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EnversHistoryApplication.class, args);
+    }
 
 }
