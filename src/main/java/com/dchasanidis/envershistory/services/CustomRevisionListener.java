c7b74717c155b8e0445d5408c8c9a678acb99771
Dimitris Chasanidis <dchasanidis@dol-guldur.speedport.ip>
2024-04-21 18:32:03 +0200

    add custom revision listener that adds the username of the one who modified the entity

---
+++ src/main/java/com/dchasanidis/envershistory/services/CustomRevisionListener.java
@@ -0,0 +1,21 @@
+package com.dchasanidis.envershistory.services;
+
+import com.dchasanidis.envershistory.entities.envers.MyRevision;
+import org.hibernate.envers.RevisionListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomRevisionListener implements RevisionListener {
+
+    @Override
+    public void newRevision(Object revisionEntity) {
+        final String principalName = getDummyUsername();
+        final MyRevision myRevision = (MyRevision) revisionEntity;
+        myRevision.setUsername(principalName);
+    }
+
+    // assume we have a proper authentication/authorization system
+    private static String getDummyUsername() {
+        return "Dummy_username";
+    }
+}
